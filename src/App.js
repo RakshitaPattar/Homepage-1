880b351eaf61ead69f4693b31f7e4a5be49a521d
RakshitaPattar <92937697+RakshitaPattar@users.noreply.github.com>
2022-01-04 12:33:15 +0530

    Backvideo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Charity from "./Components/Charity";
 import CharityPage2 from "./Components/Charity_2";
 import Footer from './Components/Footer';
+import Backvideo from "./Components/Backvideo";
 function App() {
   const title = "CHARITIES";
   const desc =
@@ -9,15 +10,13 @@ function App() {
   const image = {
     food: require("./charity_guest.png"),
   };
+  // const content = "EMPOWERING ATHLETS FOR LIFE"
 
   return (
     <div>
+      <Backvideo/>
       <Charity title_p={title} description={desc} imagesrc={image} />
-      <p>Git test phrase </p>
-      <p>Git test phrase 2</p>
-    <>
-	<Footer />
-	</>
+    	<Footer />
     </div>
   );
 }
